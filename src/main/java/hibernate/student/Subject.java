8592cbbaa272d0c988e32dcb681631c98560e44e
robert <robert.piotrowski92@gmail.com>
2018-10-06 10:00:00 +0200

    finished task 1 and 2 without menu - only from main

---
+++ src/main/java/hibernate/student/Subject.java
@@ -0,0 +1,49 @@
+package hibernate.student;
+
+
+import org.hibernate.annotations.Columns;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "SUBJECT")
+public class Subject {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idSubject;
+
+    private String subjectName;
+
+    @ManyToMany //todo join columns albo mappedBY
+    private Set<Student> students;
+
+    public Subject() {
+    }
+
+    public int getIdSubject() {
+        return idSubject;
+    }
+
+    public void setIdSubject(int idSubject) {
+        this.idSubject = idSubject;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+}
