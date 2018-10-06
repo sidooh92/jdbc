91788fdc7a727a0dfff60fc89bd25a24ffc877e2
robert <robert.piotrowski92@gmail.com>
2018-10-06 13:00:55 +0200

    finished task 1 and 2 without menu - only from main

--- src/main/java/hibernate/Main.java
+++ src/main/java/hibernate/Main.java
@@ -4,49 +4,51 @@ import hibernate.student.*;
 
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
         StudentHibernateRepository studentRepo = new StudentHibernateRepository();
         MarkHibernateRepository markRepo = new MarkHibernateRepository();
+        SubjectHibernateRepository subjectRepository = new SubjectHibernateRepository();
       //dodanie nowego studenta o id 1
         Student student = new Student();
-        student.setSurname("RobertNazwisko");
-        student.setName("Robert");
-        studentRepo.insert(student);
+        student.setIdStudent(1);
 
-        //dodanie nowego studenta o id 2
-        Student student2 = new Student();
-        student2.setSurname("Tom");
-        student2.setName("Jerry");
-        studentRepo.insert(student2);
-
-        //dodanie 2 ocen do studenta o id 1
-        markRepo.insert(new Mark(5,student));
-        markRepo.insert(new Mark(5,student));
-
-
-        markRepo.insert(new Mark(3,student2));
-        markRepo.insert(new Mark(2,student2));
-
-        //pobranie studenta o o id 1 i wyswietlenie wewnatrz metody get jego toString
-        studentRepo.get(1);
-
-        //usuniecie oceny po id
-        markRepo.delete(1);
-
-        //wyswietlenie studenta  z ocenami po usunieciu jednej oceny
-        studentRepo.get(1);
-
-
-        //pobranie i wyswietlenie wszystkich studentow z ich ocenami
-        studentRepo.getAll();
-
-
-        //pobranie i wyswietlenie wszystkich ocen
-        markRepo.getAll();
+        Subject subject = new Subject();
+        subject.setSubjectName("TEST2");
+        Set<Student> studentSet = new HashSet<>();
+        studentSet.add(student);
+        subject.setStudents(studentSet);
+        subjectRepository.insert(subject);
+//
+//        //dodanie 2 ocen do studenta o id 1
+//        markRepo.insert(new Mark(5,student));
+//        markRepo.insert(new Mark(5,student));
+//
+//
+//        markRepo.insert(new Mark(3,student2));
+//        markRepo.insert(new Mark(2,student2));
+//
+//        //pobranie studenta o o id 1 i wyswietlenie wewnatrz metody get jego toString
+//        studentRepo.get(1);
+//
+//        //usuniecie oceny po id
+//        markRepo.delete(1);
+//
+//        //wyswietlenie studenta  z ocenami po usunieciu jednej oceny
+//        studentRepo.get(1);
+//
+//
+//        //pobranie i wyswietlenie wszystkich studentow z ich ocenami
+//        studentRepo.getAll();
+//
+//
+//        //pobranie i wyswietlenie wszystkich ocen
+//        markRepo.getAll();
 
 
         HibernateUtils.closeEmFactory();
